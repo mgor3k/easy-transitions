129db486504147c1aa251a4b19d5b483f1882cdb
Maciej Górecki <maciekgorecki@icloud.com>
2020-08-29 14:22:37 +0200

    Added push transition

---
+++ Sources/EasyTransitions/Transitions/PushTransition.swift
@@ -0,0 +1,44 @@
+//
+//  Created by Maciej Gorecki on 29/08/2020.
+//
+
+#if canImport(UIKit)
+import UIKit
+
+class PushTransition: NSObject, UIViewControllerAnimatedTransitioning {
+    private let animationDuration: TimeInterval
+    
+    init(duration: TimeInterval) {
+        self.animationDuration = animationDuration
+    }
+    
+    func transitionDuration(
+        using transitionContext: UIViewControllerContextTransitioning?
+    ) -> TimeInterval {
+        animationDuration
+    }
+    
+    func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
+        let containerView = transitionContext.containerView
+        guard let toView = transitionContext.view(forKey: .to), let fromView = transitionContext.view(forKey: .from) else { return }
+        
+        containerView.addSubview(toView)
+        
+        let startingFrame = CGRect(x: 0, y: toView.frame.height, width: toView.frame.width, height: toView.frame.height)
+        toView.frame = startingFrame
+        
+        UIView.animate(withDuration: animationDuration,
+                       delay: 0,
+                       usingSpringWithDamping: 1,
+                       initialSpringVelocity: 1,
+                       options: .curveEaseOut,
+                       animations: {
+                        toView.frame = CGRect(x: 0, y: 0, width: toView.frame.width, height: toView.frame.height)
+                        fromView.frame = CGRect(x: 0, y: -fromView.frame.height, width: fromView.frame.width, height: fromView.frame.height)
+        }, completion: { _ in
+            transitionContext.completeTransition(true)
+        })
+        
+    }
+}
+#endif
