75d519bd97a802ee9f72fca1f2e3af86d761f66b
Maciej Górecki <maciekgorecki@icloud.com>
2020-08-30 10:27:24 +0200

    Added new transition type

---
+++ Sources/EasyTransitions/Transitions/TopToBottomTransition.swift
@@ -0,0 +1,67 @@
+//
+//  Created by Maciej Gorecki on 30/08/2020.
+//
+
+#if canImport(UIKit)
+import UIKit
+
+public class TopToBottomTransition: NSObject, UIViewControllerAnimatedTransitioning {
+    private let duration: TimeInterval
+    
+    public init(duration: TimeInterval) {
+        self.duration = duration
+    }
+    
+    public func transitionDuration(
+        using transitionContext: UIViewControllerContextTransitioning?
+    ) -> TimeInterval {
+        duration
+    }
+    
+    public func animateTransition(
+        using transitionContext: UIViewControllerContextTransitioning) {
+        let containerView = transitionContext.containerView
+        guard let toView = transitionContext.view(forKey: .to),
+              let fromView = transitionContext.view(forKey: .from) else {
+            return
+        }
+        
+        containerView.addSubview(toView)
+        
+        let startingFrame = CGRect(
+            x: 0,
+            y: toView.frame.height,
+            width: -toView.frame.width,
+            height: toView.frame.height
+        )
+        
+        toView.frame = startingFrame
+        
+        let animations = {
+            toView.frame = .init(
+                x: 0,
+                y: 0,
+                width: toView.frame.width,
+                height: toView.frame.height
+            )
+            
+            fromView.frame = .init(
+                x: 0,
+                y: fromView.frame.height,
+                width: fromView.frame.width,
+                height: fromView.frame.height
+            )
+        }
+        
+        let completion = {
+            transitionContext.completeTransition(true)
+        }
+        
+        UIView.springAnimate(
+            duration: duration,
+            animations: animations,
+            completion: completion
+        )
+    }
+}
+#endif
