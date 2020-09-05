47db75a050ee30d4cb03877b0e74425c52553ce6
Maciej Górecki <maciekgorecki@icloud.com>
2020-09-05 17:39:35 +0200

    Removed tests

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "EasyTransitions",
     platforms: [
-        .iOS(.v8)
+        .iOS(.v10)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
@@ -23,9 +23,6 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "EasyTransitions",
-            dependencies: []),
-        .testTarget(
-            name: "EasyTransitionsTests",
-            dependencies: ["EasyTransitions"]),
+            dependencies: [])
     ]
 )
--- Tests/LinuxMain.swift
+++
@@ -1,7 +0,0 @@
-import XCTest
-
-import EasyTransitionsTests
-
-var tests = [XCTestCaseEntry]()
-tests += EasyTransitionsTests.allTests()
-XCTMain(tests)
